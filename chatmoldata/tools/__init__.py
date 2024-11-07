d10bfda7a2d4b0cd320ed014ffcb8716c3469857
Yi Yu <58670828+YiYuDL@users.noreply.github.com>
2024-11-07 15:19:32 +0800

    Update __init__.py

--- chatmoldata/tools/__init__.py
+++ chatmoldata/tools/__init__.py
@@ -2,5 +2,5 @@ from .molecule import *
 from .rdkit import SMILES2Weight,MolSimilarity
 from .dataframe import CSV2Structre_image
 from .search import *
-from .database import Target2ID, SQLtext2CSV#
+from .database import Target2ID, Text2SQL2CSV
 #from .literature import *
